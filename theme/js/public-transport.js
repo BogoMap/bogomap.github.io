a2baa9350b247ba14a9feb569ee1a1e97fac1c34
BogoMap <contacto@bogomap.co>
2016-10-04 20:51:03 +0000

    Travis build 50 pushed to Github Pages

--- theme/js/public-transport.js
+++ theme/js/public-transport.js
@@ -17,35 +17,46 @@ along with BogoMap-rutas.  If not, see http://www.gnu.org/licenses/.
 
 var map;
 var feature;
+var scrolled=0;
 
 function load_map(url_params) {
 
   var humanitarian = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    attribution: 'Teselas © <a href="http://hot.openstreetmap.org/">Humanitarian OpenStreetMap Team</a>; Información geográfica © <a href="http://openstreetmap.org">OpenStreetMap</a>'
+    attribution: 'Geo datos © <a href="http://openstreetmap.org">OpenStreetMap</a>'
   });
   var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© Colaboradores de <a href="http://openstreetmap.org">OpenStreetMap</a>'
   });http:
   var pub_transport = L.tileLayer('http://tile.memomaps.de/tilegen/{z}/{x}/{y}.png', {
-    attribution: 'Teselas © <a href="http://memomaps.de/">MeMoMaps</a>; Información geográfica © <a href="http://openstreetmap.org">OpenStreetMap</a>'
+    attribution: 'Geo datos © <a href="http://openstreetmap.org">OpenStreetMap</a>; Teselas © <a href="http://memomaps.de/">MeMoMaps</a>'
+  });
+  var new_transport = L.tileLayer('http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png', {
+    attribution: 'Geo datos © <a href="http://openstreetmap.org">OpenStreetMap</a>; Teselas © <a href="http://thunderforest.com/">Gravitystorm</a>'
   });
   var mapbox = L.tileLayer('http://{s}.tiles.mapbox.com/v3/jaakkoh.map-4ch3dsvl/{z}/{x}/{y}.png', {
-    attribution: 'Teselas © <a href="http://mapbox.com/">Mapbox</a>; Información geográfica © <a href="http://openstreetmap.org">OpenStreetMap</a>'
+    attribution: 'Geo datos © <a href="http://openstreetmap.org">OpenStreetMap</a>; Teselas © <a href="http://mapbox.com/">Mapbox</a>'
   });
+  var osmsweden = L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
+    attribution: 'Geo datos © <a href="http://openstreetmap.org">OpenStreetMap</a>; Teselas © <a href="http://http://openstreetmap.se/">OSM Suecia</a>'
+  });
+
+
+
 
   var baseLayers = {
     "Transporte público": pub_transport,
     "Humanitarian": humanitarian,
     "OpenStreetMap": osm,
     "Mapbox": mapbox,
+    "OSM Sweden": osmsweden,
   };
 
   // Initialize map
   map = new L.map('map', {
-    center: [4.65,-74.07],
-    zoom: 13,
+    center: [5.829,-73.029],
+    zoom: 14,
     attributionControl: false,
-    layers: baseLayers[url_params.layers] || pub_transport
+    layers: baseLayers[url_params.layers] || new_transport
   });
 
   // Adding hash for position in url
@@ -55,8 +66,8 @@ function load_map(url_params) {
   L.control.attribution({position: 'bottomleft'}).addTo(map);
 
   // Adding layer functionality
-  var layers = L.control.activeLayers(baseLayers);
-  layers.setPosition('bottomleft').addTo(map);
+  //var layers = L.control.activeLayers(baseLayers);
+  //layers.setPosition('bottomleft').addTo(map);
 
 }
 
@@ -92,6 +103,58 @@ function updateUrlParameter(uri, key, value) {
     return uri + hash;  // finally append the hash as well
 }
 
+
+function toggleExtendedBusInfo() {
+  if($(".info-wrapper").hasClass("normal")){
+    openExtendedBusInfo();
+  }
+  else if ($(".info-wrapper").hasClass("expanded")){
+    closeExtendedBusInfo();
+  }
+}
+
+//
+function openExtendedBusInfo() {
+  $(".info-wrapper").removeClass("normal");
+  $(".info-wrapper").addClass("expanded");
+  $("#info-expander").removeClass("fa-chevron-down");
+  $("#info-expander").addClass("fa-chevron-up");
+}
+
+
+
+//
+function closeExtendedBusInfo() {
+
+  $(".info-wrapper").removeClass("expanded");
+  $(".info-wrapper").addClass("normal");
+  $("#info-expander").removeClass("fa-chevron-up");
+  $("#info-expander").addClass("fa-chevron-down");
+}
+
+function showBusInfo() {
+
+  // Show and fill metadata in site
+  if(!$(".info-wrapper").hasClass("normal")){
+     $(".info-wrapper").addClass("normal");
+  }
+}
+
+
+
+//
+function zoomToNode(lat, lng) {
+
+    closeExtendedBusInfo();
+
+    // define geographical bounds
+    var bounds = [[lng+0.0001, lat+0.0001], [lng-0.0001, lat-0.0001]];
+    map.fitBounds(bounds);
+    map.setZoom(18);
+}
+
+
+
 function loadBusRoute(busDetailLayerGroup, bus_number, category) {
 
   // Clear old bus line
@@ -99,14 +162,15 @@ function loadBusRoute(busDetailLayerGroup, bus_number, category) {
 
   // Define colors for transport categories
   var transportCategories = {
-    'lines-principal': '#00ff00',
-    'lines-central': '#c69c6d',
-    'lines-eastern': '#BB5BDE',
-    'lines-western': '#00ffff',
-    'lines-northern': '#f15a24',
-    'lines-southern': '#0000ff',
-
+    'lines-principal': '#45B549',
+    'lines-central': '#A8611A',
+    'lines-eastern': '#6d2fba',
+    'lines-western': '#00ADEE',
+    'lines-northern': '#F7921F',
+    'lines-southern': '#0055FF',
+    'lines-ciudadsandino': '#0076a2',
   }
+
   var myStyle = {
     color: transportCategories[category],
 
@@ -122,6 +186,7 @@ function loadBusRoute(busDetailLayerGroup, bus_number, category) {
     riseOnHover: true,
   };
 
+
   // Load data from file
   $.ajax({
     type: "GET",
@@ -130,18 +195,127 @@ function loadBusRoute(busDetailLayerGroup, bus_number, category) {
     async: true,
     cache: true,
     success: function (response) {
-        // Define content of popup
-        geojsonLayer = L.geoJson(response, {
+
+      // Clean out meta data
+      $(".stop-overview .variant-one h4").text();
+      $(".stop-overview .variant-two h4").text();
+      $(".stop-overview .variant-one ul").empty();
+
+      // Define bus stop maker
+      geojsonLayer = L.geoJson(response, {
           style: myStyle,
           pointToLayer: function (feature, latlng) {
             marker = L.circleMarker(latlng, geojsonMarkerOptions);
             return marker;
           },
+
+          // Loop through bus route elements
           onEachFeature: function (feature, layer) {
+
+            // If it's a bus stop
             if (feature.geometry.type == 'Point') {
+
+              // BogoMap: Test if the name value is defined.
+              if (typeof feature.properties.name !== "undefined"){
+
+              // Define content of popup
               layer.bindLabel(feature.properties.name, {noHide: false});
+
+              // BogoMap
+              } else {
+                name = feature.properties;
+    //            layer.bindLabel(name, {noHide: false});
+              }
+
+              // BogoMap: Test if the official_status value is defined.
+              if (typeof feature.properties.attributes !== "undefined"){
+
+              // Create list of bus stops
+              if (feature.properties.attributes.official_status == "IRTRAMMA:bus_stop") {
+                stopClass = "stop-official";
+              }
+              else if (feature.properties.attributes.official_status == "IRTRAMMA:bus_station") {
+                stopClass = "stop-station";
+              }
+              else if (feature.properties.attributes.official_status == "none") {
+                stopClass = "stop-popular";
+              }
+              else {
+                stopClass = "stop-undefined";
+              }
+
+              // BogoMap
+              } else {
+                stopClass = "stop-undefined";
+              }
+
+              if (feature.properties.name === '') {
+                feature.properties.name = '<span class="stop-unknown">Nombre desconocido</span>';
+              }
+              $('.stop-overview .variant-one ul').append('<li class="'+stopClass+'"><a href="#" onclick="zoomToNode('+feature.geometry.coordinates[0]+', '+feature.geometry.coordinates[1]+');return false;">'+feature.properties.name+'</a></li>');
             }
-            //layer.bindPopup(feature.properties.name);
+
+            // If it's the bus route
+            else if (feature.geometry.type == 'LineString' || feature.geometry.type == 'MultiLineString') {
+
+              showBusInfo();
+
+              // BogoMap: Test if the ref value is defined.
+              if (typeof feature.properties.attributes !== "undefined" && typeof feature.properties.attributes.ref !== "undefined"){
+
+              $(".info-wrapper .ref").text("Ruta " + feature.properties.attributes.ref);
+
+              // BogoMap
+              } else {
+                ref = feature.properties["@relations"][0].reltags.ref;
+                $(".info-wrapper .ref").text("Ruta " + ref);
+              }
+              // BogoMap: Test if the from value is defined.
+              if (typeof feature.properties.attributes !== "undefined" && typeof feature.properties.attributes.from !== "undefined"){
+
+              $(".info-wrapper .from").text(feature.properties.attributes.from);
+
+              // BogoMap
+              } else {
+                from = feature.properties["@relations"][0].reltags.from;
+                $(".info-wrapper .from").text(from);
+              }
+              // BogoMap: Test if the to value is defined.
+              if (typeof feature.properties.attributes !== "undefined" && typeof feature.properties.attributes.to !== "undefined"){
+
+              $(".info-wrapper .to").text(feature.properties.attributes.to);
+
+              // BogoMap
+              } else {
+                to = feature.properties["@relations"][0].reltags.to;
+                $(".info-wrapper .to").text(to);
+              }
+              // BogoMap: Test if the operator value is defined.
+              if (typeof feature.properties.attributes !== "undefined" && typeof feature.properties.attributes.operator !== "undefined"){
+
+              $(".info-wrapper .operator").text("Operada por:  " + feature.properties.attributes.operator);
+
+              // BogoMap
+              } else {
+                operator = feature.properties["@relations"][0].reltags.operator;
+                $(".info-wrapper .operator").text(operator);
+              }
+
+              // BogoMap: Test if the from value is defined.
+              if (typeof feature.properties.attributes !== "undefined" && typeof feature.properties.attributes.from !== "undefined" && typeof feature.properties.attributes.to !== "undefined"){
+
+              $(".stop-overview .variant-one h4").text(feature.properties.attributes.from + " -> " + feature.properties.attributes.to);
+              $(".stop-overview .variant-two h4").text(feature.properties.attributes.to + " -> " + feature.properties.attributes.from);
+
+              // BogoMap
+              } else {
+                to = feature.properties["@relations"][0].reltags.to;
+                from = feature.properties["@relations"][0].reltags.from;
+                $(".stop-overview .variant-one h4").text(from + " -> " + to);
+                $(".stop-overview .variant-two h4").text(to + " -> " + from);
+              }
+
+            }
         }});
         busDetailLayerGroup.addLayer(geojsonLayer);
     },
@@ -158,18 +332,60 @@ function loadBusRoute(busDetailLayerGroup, bus_number, category) {
     async: true,
     cache: true,
     success: function (response) {
-        // Define content of popup
-        geojsonLayer = L.geoJson(response, {
+
+      // Clean out meta data
+      $(".stop-overview .variant-two ul").empty();
+
+      // Define bus stop maker
+      geojsonLayer = L.geoJson(response, {
           style: myStyle,
           pointToLayer: function (feature, latlng) {
             marker = L.circleMarker(latlng, geojsonMarkerOptions);
             return marker;
           },
+
+          // Loop through bus route elements
           onEachFeature: function (feature, layer) {
+
+            // If it's a bus stop
             if (feature.geometry.type == 'Point') {
+
+              // BogoMap: Test if the name value is defined.
+              if (typeof feature.properties.name !== "undefined"){
+
+              // Define content of popup
               layer.bindLabel(feature.properties.name, {noHide: false});
+
+              // BogoMap
+              }
+
+              // BogoMap: Test if the official_status value is defined.
+              if (typeof feature.properties.attributes !== "undefined"){
+
+              // Create list of bus stops
+              if (feature.properties.attributes.official_status == "IRTRAMMA:bus_stop") {
+                stopClass = "stop-official";
+              }
+              else if (feature.properties.attributes.official_status == "IRTRAMMA:bus_station") {
+                stopClass = "stop-station";
+              }
+              else if (feature.properties.attributes.official_status == "none") {
+                stopClass = "stop-popular";
+              }
+              else {
+                stopClass = "stop-undefined";
+              }
+
+              // BogoMap
+              } else {
+                stopClass = "stop-undefined";
+              }
+
+              if (feature.properties.name === '') {
+                feature.properties.name = '<span class="stop-unknown">Nombre desconocido</span>';
+              }
+              $('.stop-overview .variant-two ul').append('<li class="'+stopClass+'"><a href="#" onclick="zoomToNode('+feature.geometry.coordinates[0]+', '+feature.geometry.coordinates[1]+');return false;">'+feature.properties.name+'</a></li>');
             }
-            //layer.bindPopup(feature.properties.name);
         }});
         busDetailLayerGroup.addLayer(geojsonLayer);
     },
@@ -184,22 +400,27 @@ $(document).ready(function() {
   // Obtain parameters from url
   var url_params = get_params();
 
+  // Load maps (with predefined route if given)
   load_map(url_params);
-
   var busDetailLayerGroup = new L.LayerGroup();
   busDetailLayerGroup.addTo(map);
   if (typeof url_params.ruta !== 'undefined') {
     bus_number = url_params.ruta;
-    category = $(this).find(".ruta-" + url_params.ruta).parent().attr("class");
+    category = $(this).find(".ruta-" + url_params.ruta).attr("class").match(/\lines-\w+/i).pop();
     loadBusRoute(busDetailLayerGroup, bus_number, category);
   }
 
+  // Show or hide extended bus info
+  $("#info-expander ").click (function(e) {
+    toggleExtendedBusInfo();
 
+  });
+
+  // Switch highligted bus route
   $(".bus-line-link").click (function(e) {
 
     // Do not reload page
     e.preventDefault();
-    console.log(e);
 
     // Mark link as active
     $('a.bus-active').removeClass('bus-active');
@@ -211,7 +432,23 @@ $(document).ready(function() {
       uri = updateUrlParameter(window.location.href, 'ruta', this.text);
       window.history.pushState({path:uri},'',uri);
     }
-    console.log(this);
-    loadBusRoute(busDetailLayerGroup, this.text, $(this).parent().attr("class"));
+    loadBusRoute(busDetailLayerGroup, this.text, $(this).attr("class").match(/\lines-\w+/i));
+  });
+
+
+  // Allow scrolling through bus route navigation by buttons
+  $("#bus-lines-control-top").on("click" ,function(){
+    scrolled=scrolled+240;
+    $("#bus-lines").animate({
+      scrollTop:  scrolled
+    });
+  });
+
+  $("#bus-lines-control-bottom").on("click" ,function(){
+    scrolled=scrolled-240;
+    $("#bus-lines").animate({
+      scrollTop:  scrolled
+    });
   });
 });
+
